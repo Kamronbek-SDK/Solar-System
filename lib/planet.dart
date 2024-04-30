5faed418ea3c9f4047ea637da2b61c208a401338
samandar-me <samandar>
2024-04-30 16:43:59 +0500

    Our Solar System - Milky Way.

--- lib/planet.dart
+++ lib/planet.dart
@@ -8,34 +8,35 @@ class Planet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            title: Text(thing.name, style: const TextStyle(fontSize: 20),),
-            flexibleSpace: FlexibleSpaceBar(
-              centerTitle: true,
-              background: Image.asset("asset/solar/${thing.image}.jpg", fit: BoxFit.cover, width: 200, height: 200,),
-            ),
-            expandedHeight: 350,
+      body: Stack(
+        children: [
+          Image.asset('asset/solar/universe2.jpg', width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,fit: BoxFit.cover,
           ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              childCount: 1,
-                (context, index) => Stack(
-                    children: [
-                      Image.asset('asset/solar/universe2.jpg', width: MediaQuery.of(context).size.width,
-                        height: MediaQuery.of(context).size.height,fit: BoxFit.cover,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(10),
-                        child: Text(thing.description),
-                      )
-                    ]
-                )
+          CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              pinned: true,
+              title: Text(thing.name, style: const TextStyle(fontSize: 20),),
+              flexibleSpace: FlexibleSpaceBar(
+                centerTitle: true,
+                background: Image.asset("asset/solar/${thing.image}.jpg", fit: BoxFit.cover, width: 200, height: 200,),
+              ),
+              expandedHeight: 350,
+            ),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                childCount: 1,
+                  (context, index) => Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Text(thing.description),
+                  )
+              )
             )
-          )
-          //SizedBox(child: Image.asset('asset/solar/universe2.jpg', width: double.infinity, height: double.infinity, fit: BoxFit.cover,)),
-        ],
+            //SizedBox(child: Image.asset('asset/solar/universe2.jpg', width: double.infinity, height: double.infinity, fit: BoxFit.cover,)),
+          ],
+        ),
+        ]
       ),
     );
   }
--- lib/planet_item.dart
+++ lib/planet_item.dart
@@ -9,35 +9,47 @@ class PlanetItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-        onTap: onClick,
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Ink(
-            width: MediaQuery.of(context).size.width,
-            height: 200,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(12),
-              //color: const Color(0xFFdba07b),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(20),
-              child: Row(
-                children: [
-                  SizedBox(
-                    width: 140,
-                    height: 140,
-                    child: CircleAvatar(
-                      backgroundImage: AssetImage("asset/solar/${planet.avatar}.jpg"),
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          //color: const Color(0xFFdba07b),
+        ),
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+              onTap: onClick,
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Ink(
+                  width: MediaQuery.of(context).size.width,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                   border: Border.all(color: const Color(0xFFdba07b), width: 2)
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          width: 140,
+                          height: 140,
+                          child: CircleAvatar(
+                            backgroundImage: AssetImage("asset/solar/${planet.avatar}.jpg"),
+                          ),
+                        ),
+                        const Gap(20),
+                        Text(planet.name, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),)
+                      ],
                     ),
                   ),
-                  const Gap(20),
-                  Text(planet.name, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),)
-                ],
+                ),
               ),
             ),
-          ),
         ),
-      );
+      ),
+    );
   }
 }
